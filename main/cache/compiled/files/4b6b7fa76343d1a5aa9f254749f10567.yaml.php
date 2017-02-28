2dc930ba0606ba6d677098fbad9d88321348d5f9
Johnny Bender <johnphillipbender@gmail.com>
2017-02-28 02:18:52 -0600

    fixed untracked files

--- main/cache/compiled/files/4b6b7fa76343d1a5aa9f254749f10567.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Users/jb/Documents/Code#RGV/codeRGVweb/grav/user/config/security.yaml',
-    'modified' => 1488258262,
-    'data' => [
-        'salt' => '34vhjkbJgD9mRu'
-    ]
-];
